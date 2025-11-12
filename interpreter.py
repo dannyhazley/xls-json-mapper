3761a45085632e49caa317f28338e3dab3ba8538
Danny Hazley <54882850+dannyhazley@users.noreply.github.com>
2025-11-12 17:59:40 +0000

    Added Dynamic functionality for enum creation

--- interpreter.py
+++ interpreter.py
@@ -1,18 +1,28 @@
 import pandas as pd
 import uuid
 
-def injestFile(FilePath):
+def injestFile(FilePath, actionBool):
     dfRaw = pd.read_excel(FilePath)
 
     for index, row in dfRaw.iterrows():
-        id = uuid.uuid4()
-        code = "new Action = ({id}, {description}, Map.of(\n".format(id=id, description=dfRaw.iloc[index][2])
-        print("// {action}: ".format(action=dfRaw.iloc[index, 2]))
-        for x in range (3, len(dfRaw.columns)):
-            if pd.notna(row[x]):
-                code += "\t\"{key}\", {value},\n".format(key=dfRaw.columns[x], value=row[x])
-            x += 1
-        code += "));"
-        print(code)
-        print("\n\n//----------------------\n\n")
+        if actionBool:
+            id = uuid.uuid4()
+            code = "new Action = ({id}, {description}, Map.of(\n".format(id=id, description=dfRaw.iloc[index].iloc[2])
+            print("// {action}: ".format(action=dfRaw.iloc[index].iloc[2]))
+            for x in range (3, len(dfRaw.columns)):
+                if pd.notna(row[x]):
+                    code += "\t\"{key}\", {value},\n".format(key=dfRaw.columns[x], value=row.iloc[x])
+                x += 1
+            code += "));"
+            print(code)
+            print("\n\n//----------------------\n\n")
+        else:
+            code = "{enum}(\"{description}\", Map.of(\n".format(enum=dfRaw.iloc[index].iloc[0].to_string().upper(), description=dfRaw.iloc[index].iloc[1])
+            print("// {enum}: ".format(enum=dfRaw.iloc[index].iloc[0].to_string().upper()))
+            for x in range (2, len(dfRaw.columns)):
+                if pd.notna(row[x]):
+                    code += "\t\"{key}\", {value},\n".format(key=dfRaw.columns[x], value=row.iloc[x])
+                x += 1
+            code += ")),"
+            print(code)
 
--- main.py
+++ main.py
@@ -2,11 +2,27 @@ import interpreter
 
 
 print("---The following program is for use with a CSC2058 Group 7 Spreadsheet.---\n")
-print("Please ensure your .xlsx is the following shape:")
-print("Column A: ID (Replaced with UUID4), Column B: Category, Column C: Description, Column D onwards: Effect Keys")
-print("Row A: Headings.  For Columns D onward please use the Key, Row B onwards: Values")
+
+
+action = input("Do you want to interpret an action spreadsheet (a), or an event spreadsheet (e)?: ")
+actionBool = True
+while True:
+    if action == "a":
+        print("Please ensure your .xlsx is the following shape:")
+        print("Column A: ID (Replaced with UUID4), Column B: Category, Column C: Description, Column D onwards: Effect Keys")
+        print("Row A: Headings.  For Columns D onward please use the Key, Row B onwards: Values")
+        actionBool = True
+        break
+    elif action == "e":
+        print("Please ensure your .xlsx is the following shape:")
+        print("Column A: Enum Name, Column B: Description, Column C onwards: Effect Keys")
+        print("Row A: Headings.  For Columns C onward please use the Key, Row B onwards: Values")
+        actionBool = False
+        break
+    else:
+        action = input("Please enter \"a\" or \"e\": ")
 
 fPath = input("Please enter the file path: ")
 print("\n\n----------Outputting Maps----------\n\n\n\n\n\n")
-interpreter.injestFile(fPath)
+interpreter.injestFile(fPath, actionBool)
 
